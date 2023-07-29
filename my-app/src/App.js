da29d4a56bcbfcaa4e9924052d52bf7df00ae954
Aseru Kevin Ziyada <109363782+lucy-kevin@users.noreply.github.com>
2023-07-29 11:43:52 +0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,34 @@
+
 import { useState } from 'react';
 import './App.css';
-
-function ListItems({ ints, addValue }) {
-  const increment = 3;
-
-  return (
+function ListItems({ints, addValues}){
+ const increment = 3;
+  return(
     <>
-      <button onClick={() => addValue(increment)}>Add Item</button>
-      {
-        ints?.map(id => {
-          return (
-            <li key={id}>{id}</li>
-          );
-        })
-      }
+    <button onClick={()=> addValues(increment)}>add item</button>
+    {
+
+      ints.map(id => {
+        return (
+          <li key={id}>{id}</li>
+        )
+      })
+    }
+  
     </>
-  );
-}
-
-function App() {
-  const [ints, setInts] = useState([1, 2, 3]);
-
-  function addValue(incrementValue) {
-    const newVal = Math.max(...ints) + incrementValue;
-    setInts([...ints, newVal]);
+  )
   }
+function App() {
+ // const ints = [1,2,3];
+ const[ints, setInts] = useState([1,2,3]);
+  function addValues(incrementValue){
+    const newVal = Math.max(...ints) + incrementValue;
+    setInts([...ints,newVal]);
 
+  }
   return (
     <ul>
-      <ListItems ints={ints} addValue={addValue} />
+      <ListItems ints={ints} addValues = {addValues}/>
     </ul>
   );
 }
