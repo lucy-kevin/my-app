053a0705695335b28b9ce071527a0c4cdb2ab414
Aseru Kevin Ziyada <109363782+lucy-kevin@users.noreply.github.com>
2023-08-04 16:15:24 +0300

    1

--- my-app/src/main-page/header.js
+++ my-app/src/main-page/header.js
@@ -1,27 +1,13 @@
 import logo from "./Icon2.png";
-import { Component } from "react";
- class Header extends Component{
-    render(){
-        return(
-            <header className = "row">
-                    <div className = "col-md-5">
-                        <img src= {logo} className= "logo" alt ="logo"/>
-                    </div>
-                    <div className="col-md-7 mt-5 subtitle">{this.props.subtitles}
-                    </div>
-                </header>
-            );
-        
-    }
- }
-// const Header = ({subtitles}) => (
-//     <header className = "row">
-//         <div className = "col-md-5">
-//             <img src= {logo} className= "logo" alt ="logo"/>
-//         </div>
-//         <div className="col-md-7 mt-5 subtitle">
-//             {props.subtitles}
-//         </div>
-//     </header>
-// );
+ 
+const Header = ({subtitles}) => (
+    <header className = "row">
+        <div className = "col-md-5">
+            <img src= {logo} className= "logo" alt ="logo"/>
+        </div>
+        <div className="col-md-7 mt-5 subtitle">
+            {subtitles}
+        </div>
+    </header>
+);
 export default Header;
