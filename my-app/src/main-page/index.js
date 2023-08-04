640e99c7da62549e5955062ddb02267c06151f69
Aseru Kevin Ziyada <109363782+lucy-kevin@users.noreply.github.com>
2023-08-04 17:52:57 +0300

    w

--- my-app/src/main-page/index.js
+++ my-app/src/main-page/index.js
@@ -1,8 +1,10 @@
-
+import featuredHouse from './featured-house';
 import './main-page.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import logo from "./Icon2.png";
 import Header  from './header';
+import {BrowserRouter as Router, Route, Switch }from "react-router-dom";
+import FeaturedHouse from './featured-house';
 
 function App(){
   const [allHouses, setAllHouses] = useState([]);
@@ -15,18 +17,30 @@ function App(){
     };
     fetchHouses();
   },[]);
+
+  const featuredHouse = useMemo(()=>{
+    if(allHouses.length){
+      const randomIndex = Math.floor(Math.random()* allHouses.length);
+       return allHouses[randomIndex];
+    }
+  },[allHouses])
   //load data
-  let featuredHouse ={};
-  if(allHouses.length){
-    const randomIndex = Math.floor(Math.random()* allHouses.length);
-    featuredHouse = allHouses[randomIndex];
-  }
+
+  
  
   return(
-    <div className='container'>
-      <Header subtitles= "Providing houses all over the world"
-      />
-    </div>
+ 
+    <Router>
+      <div className="container">
+        <Header subtitles="Providing houses all over the world"/>
+      
+      <Switch>
+        <Route path="/">
+          <FeaturedHouse house ={featuredHouse}></FeaturedHouse>
+        </Route>
+      </Switch>
+      </div>
+    </Router>
   );
 }
 export default App;
